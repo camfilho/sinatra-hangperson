0213662a2e041649f4389206344c519f4a565516
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2020-10-25 21:13:24 -0300

    initial commit

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,6 +2,7 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
 
   # Get a word from remote "random word" service
 
@@ -10,6 +11,35 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @current_letter = ''
+  end
+
+  def guess(letter)
+    @current_letter = letter.to_s.downcase
+
+    throw ArgumentError if invalid_character?
+
+
+    return false if been_guessed?
+
+    update_guesses
+
+    true
+  end
+
+  def word_with_guesses
+    word.chars.map { |char| guesses.include?(char) ? char : '-' }.join('')
+  end
+
+  def check_win_or_lose
+    @status = :play
+    @status = :win if word_with_guesses == word
+
+    @status = :lose if wrong_guesses.size > 6
+
+    @status
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -24,4 +54,19 @@ class HangpersonGame
     }
   end
 
+  private
+
+  def been_guessed?
+    guesses.include?(@current_letter) || wrong_guesses.include?(@current_letter)
+  end
+
+  def invalid_character?
+    !('a'..'z').include? @current_letter
+  end
+
+  def update_guesses
+    return self.guesses = guesses + @current_letter if word.include? @current_letter
+
+    self.wrong_guesses = wrong_guesses + @current_letter
+  end
 end
