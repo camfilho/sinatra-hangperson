7f2d65618dbb2c2b789de15e043f59d0fcad51a7
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2020-10-27 22:59:38 -0300

    make a guess

--- app.rb
+++ app.rb
@@ -38,8 +38,13 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      letter = params[:guess].to_s[0]
+      flash[:message] = 'You have already used that letter.' unless @game.guess letter
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess.'
+    end
+
     redirect '/show'
   end
   
@@ -49,7 +54,6 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
     erb :show # You may change/remove this line
   end
   
